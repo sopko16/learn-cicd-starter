b5108a976d6ac158ea4e07a3aa6fdd75b4816db6
sopko16 <sopko.16@googlemail.com>
2026-09-10 23:43:17 +0000

    Ch 2, #1: add intial tests

--- README.md
+++ README.md
@@ -23,3 +23,11 @@ go build -o notely && ./notely
 You do *not* need to set up a database or any interactivity on the webpage yet. Instructions for that will come later in the course!
 
 MIKE's version of Bootdev's Notely app.
+
+## Tests:
+
+run with:
+
+```bash
+go test ./...
+```
--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -14,7 +14,8 @@ func GetAPIKey(headers http.Header) (string, error) {
 	if authHeader == "" {
 		return "", ErrNoAuthHeaderIncluded
 	}
-	splitAuth := strings.Split(authHeader, " ")
+	// splitAuth := strings.Split(authHeader, " ")
+	splitAuth := strings.Fields(authHeader)
 	if len(splitAuth) < 2 || splitAuth[0] != "ApiKey" {
 		return "", errors.New("malformed authorization header")
 	}
