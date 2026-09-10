4b459292c04ccba3f8b6a3e7c5b7d6f6e7303594
sopko16 <sopko.16@googlemail.com>
2026-09-10 23:53:03 +0000

    revert intentional code error

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -16,7 +16,7 @@ func GetAPIKey(headers http.Header) (string, error) {
 	}
 	// splitAuth := strings.Split(authHeader, " ")
 	splitAuth := strings.Fields(authHeader)
-	if len(splitAuth) < 2 || splitAuth[0] != "Bearer" {
+	if len(splitAuth) < 2 || splitAuth[0] != "ApiKey" {
 		return "", errors.New("malformed authorization header")
 	}
 
